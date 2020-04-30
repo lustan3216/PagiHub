0996d7ad7d4da4b2b2fe2f2cbaa164128cee62d9
lululala <et3216@gmail.com>
2020-04-30 20:41:55 +0800

    remove popover and controller

--- src/components/Templates/GridItemChild.vue
+++ src/components/Templates/GridItemChild.vue
@@ -1,45 +1,18 @@
 <template>
   <div
     :style="innerStyles.default"
-    class="h-100"
+    class="h-100 border-box"
   >
-    <controller-layer
-      v-if="firstChild"
-      :id="firstChild.id"
-      class="border-box"
-    >
+    <controller-layer :id="currentNoe.id">
       <async-component
-        :tag="firstChild.tag"
-        :id="firstChild.id"
+        :tag="currentNoe.tag"
+        :id="currentNoe.id"
       />
 
-      <template v-if="isDraftMode">
-        <el-popover
-          v-if="mounted && !isExample && !isAnimating"
-          :reference="parentEl"
-          :placement="firstChild.canNewItem ? 'top' : 'right'"
-          trigger="hover"
-        >
-          <node-controller :id="firstChild.id" />
-        </el-popover>
-
-        <portal-target :name="`GridItemChild${firstChild.id}`" />
-      </template>
-    </controller-layer>
-
-    <controller-layer
-      v-else
-      :id="id"
-      class="flex-center border-box"
-    >
-      <template v-if="isDraftMode">
-        <portal-target :name="`GridItemChild${id}`" />
-        <node-controller
-          v-if="!isExample"
-          :id="id"
-          class="h-100"
-        />
-      </template>
+      <portal-target
+        v-if="isDraftMode"
+        :name="`GridItemChild${currentNoe.id}`"
+      />
     </controller-layer>
   </div>
 </template>
@@ -49,14 +22,12 @@ import { mapState } from 'vuex'
 import childrenMixin from './mixins/children'
 import nodeMixin from './mixins/node'
 import ControllerLayer from '../TemplateUtils/ControllerLayer'
-import NodeController from '../TemplateUtils/NodeController'
 import AsyncComponent from '../TemplateUtils/AsyncComponent'
 
 export default {
   name: 'GridItemChild',
   components: {
     ControllerLayer,
-    NodeController,
     AsyncComponent
   },
   mixins: [childrenMixin, nodeMixin],
@@ -67,20 +38,12 @@ export default {
   },
   computed: {
     ...mapState('app', ['isAnimating']),
+    currentNoe() {
+      return this.firstChild || this.node
+    },
     firstChild() {
       return this.innerChildren[0]
-    },
-    parentEl() {
-      return (
-        this.$parent &&
-        this.$parent.$el &&
-        this.$parent.$el.setAttribute &&
-        this.$parent.$el
-      )
     }
-  },
-  mounted() {
-    this.mounted = true
   }
 }
 </script>
