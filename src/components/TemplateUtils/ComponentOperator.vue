3edd818afed810a209ca3d16875ce93f9e955b30
lululala <et3216@gmail.com>
2020-10-21 19:07:05 +0800

    fix compoennt operator height

--- src/components/TemplateUtils/ComponentOperator.vue
+++ src/components/TemplateUtils/ComponentOperator.vue
@@ -277,7 +277,7 @@ export default {
           left,
           top,
           width: width - 2,
-          height: height - 2,
+          height: height,
           visible: true
         })
       })
