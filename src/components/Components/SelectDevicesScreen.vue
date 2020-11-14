80574dbadcc9d16e1b30cbff303d4ab4a28993b9
lululala <et3216@gmail.com>
2020-11-14 11:14:53 +0800

    deivce select refactor

--- src/components/Components/SelectDevicesScreen.vue
+++ src/components/Components/SelectDevicesScreen.vue
@@ -1,17 +1,31 @@
 <template>
   <el-dropdown
-    split-button
     size="small"
-    @command="canvasWidthChange"
+    split-button
+    type
+    @click="$emit('buttonClick')"
   >
+    <i class="el-icon-refresh-left" />
+    <span
+      class="gray-font font-12"
+      style="width: 75px; display: inline-block;"
+    >
+      {{ width }} x {{ height }}
+    </span>
 
     <el-dropdown-menu slot="dropdown">
       <el-dropdown-item
-        v-for="option in options"
-        :key="option"
-        :command="option"
+        v-for="option in deviceOptions"
+        :key="option.name"
+        :value="option.name"
+        @click.native="$emit('itemClick', option)"
       >
-        {{ option }}
+        <div class="justify-between">
+          <span class="gray-font">{{ option.name }}</span>
+          <span
+            class="m-l-15 gray-font"
+          >{{ option.w }} X {{ option.h }}</span>
+        </div>
       </el-dropdown-item>
     </el-dropdown-menu>
   </el-dropdown>
@@ -20,19 +34,39 @@
 <script>
 export default {
   name: 'SelectDevicesScreen',
-  data() {
-    const options = [
-      'Custom',
-      '1920px Computer',
-      '1200px Computer',
-      '996px  Computer',
-      '768px  Tablet',
-      '320px  Mobile'
-    ]
-
-    return {
-      current: options[0],
-      options
+  props: {
+    width: {
+      type: Number,
+      required: true
+    },
+    height: {
+      type: Number,
+      required: true
+    }
+  },
+  computed: {
+    deviceOptions() {
+      return [
+        { name: 'iMac', w: 2560, h: 1440 },
+        { name: 'MacBook Pro', w: 1440, h: 900 },
+        { name: 'Surface Pro 4', w: 1368, h: 912 },
+        { name: 'Surface Pro 3', w: 1440, h: 960 },
+        { name: 'iPhone 11 Pro Max', w: 414, h: 896 },
+        { name: 'iPhone 11 Pro', w: 375, h: 812 },
+        { name: 'iPhone 11', w: 414, h: 896 },
+        { name: 'iPhone 8 Plus', w: 414, h: 736 },
+        { name: 'iPhone 8', w: 375, h: 667 },
+        { name: 'iPhone 8 SE', w: 320, h: 568 },
+        { name: 'Google Pixel 4 XL', w: 360, h: 760 },
+        { name: 'Google Pixel 4', w: 360, h: 769 },
+        { name: 'Google Pixel 3 XL', w: 411, h: 846 },
+        { name: 'Google Pixel 3', w: 411, h: 822 },
+        { name: 'iPad Pro 12.9"', w: 1024, h: 1366 },
+        { name: 'iPad Pro 11"', w: 834, h: 1194 },
+        { name: 'iPad Air 10.5"', w: 834, h: 1112 },
+        { name: 'iPad 10.2"', w: 810, h: 1080 },
+        { name: 'iPad Mini', w: 768, h: 1024 }
+      ]
     }
   }
 }
--- src/components/Layout/ViewPort.vue
+++ src/components/Layout/ViewPort.vue
@@ -82,36 +82,12 @@
 
         <el-divider direction="vertical" />
 
-        <el-dropdown
-          size="small"
-          split-button
-          type
-          @click="setBoundaryRect"
-        >
-          <i class="el-icon-refresh-left" />
-          <span
-            class="gray-font font-12"
-            style="width: 75px; display: inline-block;"
-          >
-            {{ style.w }} x {{ style.h }}
-          </span>
-
-          <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item
-              v-for="option in deviceOptions"
-              :key="option.name"
-              :value="option.name"
-              @click.native="setSize({ w: option.w, h: option.h })"
-            >
-              <div class="justify-between">
-                <span class="gray-font">{{ option.name }}</span>
-                <span
-                  class="m-l-15 gray-font"
-                >{{ option.w }} X {{ option.h }}</span>
-              </div>
-            </el-dropdown-item>
-          </el-dropdown-menu>
-        </el-dropdown>
+        <select-devices-screen
+          :width="style.w"
+          :height="style.h"
+          @buttonClick="setBoundaryRect"
+          @itemClick="setSize({ w: $event.w, h: $event.h })"
+        />
 
         <el-button
           size="small"
@@ -131,38 +107,18 @@ import { mapState, mapMutations, mapGetters } from 'vuex'
 import { Popover } from 'element-ui'
 import SettingBreakpoints from '../Setup/EditorSetting/SettingBreakpoints'
 import ButtonDevice from '../Components/ButtonDevice'
+import SelectDevicesScreen from '../Components/SelectDevicesScreen'
 import { directive } from '@/directive/freeView'
 import { getRectWithoutPadding } from '@/utils/style'
 import { globalDebounce } from '@/utils/tool'
-
-export const DEVICE_OPTIONS = [
-  { name: 'iMac', w: 2560, h: 1440 },
-  { name: 'MacBook Pro', w: 1440, h: 900 },
-  { name: 'Surface Pro 4', w: 1368, h: 912 },
-  { name: 'Surface Pro 3', w: 1440, h: 960 },
-  { name: 'iPhone 11 Pro Max', w: 414, h: 896 },
-  { name: 'iPhone 11 Pro', w: 375, h: 812 },
-  { name: 'iPhone 11', w: 414, h: 896 },
-  { name: 'iPhone 8 Plus', w: 414, h: 736 },
-  { name: 'iPhone 8', w: 375, h: 667 },
-  { name: 'iPhone 8 SE', w: 320, h: 568 },
-  { name: 'Google Pixel 4 XL', w: 360, h: 760 },
-  { name: 'Google Pixel 4', w: 360, h: 769 },
-  { name: 'Google Pixel 3 XL', w: 411, h: 846 },
-  { name: 'Google Pixel 3', w: 411, h: 822 },
-  { name: 'iPad Pro 12.9"', w: 1024, h: 1366 },
-  { name: 'iPad Pro 11"', w: 834, h: 1194 },
-  { name: 'iPad Air 10.5"', w: 834, h: 1112 },
-  { name: 'iPad 10.2"', w: 810, h: 1080 },
-  { name: 'iPad Mini', w: 768, h: 1024 }
-]
 
 export default {
   name: 'ViewPort',
   components: {
     SettingBreakpoints,
     ButtonDevice,
-    ElPopover: Popover
+    ElPopover: Popover,
+    SelectDevicesScreen
   },
   directives: {
     FreeView: directive
@@ -198,9 +154,6 @@ export default {
     },
     interact() {
       return interactjs(this.targetEl)
-    },
-    deviceOptions() {
-      return DEVICE_OPTIONS
     }
   },
   watch: {
